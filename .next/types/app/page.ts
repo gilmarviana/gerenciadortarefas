b8ac874302c98e7233f43826ff0e4b2e5e330477
Gilmar Viana <70184391+gilmarviana@users.noreply.github.com>
2025-07-29 01:57:31 -0300

    .

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: E:\Desenvolvimento\tarefas\src\app\page.tsx
+// File: E:\Desenvolvimento\taskk\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
